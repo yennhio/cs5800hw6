df952c2e956758fcbad2f14fe2a58c66e9c5c26b
Yennhi Dang <nhidangn@gmail.com>
2023-05-05 16:02:15 -0700

    first commit

---
+++ Snack.java
@@ -0,0 +1,14 @@
+public class Snack {
+
+    String name;
+    double price;
+    int quantity;
+
+    public Snack (String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    
+}
